ba26c3b17cdcdf3b20e741e01ecd6efdb4b7a3c6
Anne Marte Hjemås <kons-ahj@digdir.no>
2024-05-07 08:45:21 +0200

    ID-4228: sentinel config (#20)
    * med sentinel config

--- eidas-redis/src/main/java/no/idporten/eidas/redis/cache/RedisConfig.java
+++ eidas-redis/src/main/java/no/idporten/eidas/redis/cache/RedisConfig.java
@@ -5,12 +5,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -26,17 +26,47 @@ public class RedisConfig {
 
     private String issuerName = "eidas-proxy";
 
+    private String sentinelMaster;
+    private String sentinelNodes;
+
+
     public int getRedisPort() {
         return Integer.parseInt(redisPort);
     }
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisHost, getRedisPort());
-        if(StringUtils.hasText(redisPassword)) {
+        if(StringUtils.isEmpty(sentinelNodes)) {
+            if(StringUtils.isEmpty(redisPort)|| StringUtils.isEmpty(redisHost)) {
+                throw new IllegalArgumentException("Missing sentinel configuration");
+            }
+            LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisHost, getRedisPort());
+            if (StringUtils.hasText(redisPassword)) {
+                lettuceConnectionFactory.setPassword(redisPassword);
+            }
+            return lettuceConnectionFactory;
+        } else {
+            return createSentinelConnectionFactory();
+        }
+    }
+
+    private LettuceConnectionFactory createSentinelConnectionFactory() {
+        if(StringUtils.isEmpty(sentinelMaster) || StringUtils.isEmpty(sentinelNodes) || StringUtils.isEmpty(redisPassword)){
+            throw new IllegalArgumentException("Missing sentinel configuration");
+        }
+        List<String> nodes = Arrays.asList(sentinelNodes.split(","));
+        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
+                .master(sentinelMaster);
+
+        for (int i = 1; i < nodes.size(); i++) {
+            String[] parts = nodes.get(i).split(":");
+            sentinelConfig.sentinel(parts[0], Integer.parseInt(parts[1]));
+        }
+
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(sentinelConfig);
+        if (StringUtils.hasText(redisPassword)) {
             lettuceConnectionFactory.setPassword(redisPassword);
         }
         return lettuceConnectionFactory;
     }
-
 
 }
